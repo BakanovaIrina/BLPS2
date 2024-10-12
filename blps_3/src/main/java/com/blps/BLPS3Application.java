0a22a886dd4c36a9e3e7a6c9ece48d3eaa9bc1ab
Ekaterina Firstova <evfirstova@avito.ru>
2024-10-12 14:21:58 +0300

    bpls4

--- blps_3/src/main/java/com/blps/BLPS3Application.java
+++ blps_3/src/main/java/com/blps/BLPS3Application.java
@@ -3,7 +3,14 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+
 @SpringBootApplication
+@EnableProcessApplication
 public class BLPS3Application {
     public static void main(String[] args) {
         SpringApplication.run(BLPS3Application.class, args);
